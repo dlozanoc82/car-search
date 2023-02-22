b476d5c10b68f3055330a3cae942cf3dad5f65d2
Daniel <dlozanoc862@gmail.com>
2023-02-21 20:03:10 -0500

    [ADD] Se crea un objeto para filtrar los datos, se agrega el evento change para guardar la informacion del e.target.value

--- js/app.js
+++ js/app.js
@@ -1,8 +1,56 @@
 // Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+const resultado = document.querySelector('#resultado');
 const max = new Date().getFullYear();
 const min = max -10
+
+// Generar un objeto con la busqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: '',
+}
+
+// Listeners de Busqueda
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+})
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+})
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+})
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+})
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+})
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+})
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
+})
+
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
